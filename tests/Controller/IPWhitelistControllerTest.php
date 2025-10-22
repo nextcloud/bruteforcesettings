10172f009e2235ac657c1aaeca3668401b29848f
Joas Schilling <coding@schilljs.com>
2025-10-22 17:05:18 +0200

    ci(psalm): Add psalm CI
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/Controller/IPWhitelistControllerTest.php
+++ tests/Controller/IPWhitelistControllerTest.php
@@ -24,7 +24,7 @@ class IPWhitelistControllerTest extends TestCase {
 	/** @var IPWhitelistController */
 	private $controller;
 
-	public function setUp() {
+	public function setUp(): void {
 		parent::setUp();
 
 		$this->config = $this->createMock(IConfig::class);
