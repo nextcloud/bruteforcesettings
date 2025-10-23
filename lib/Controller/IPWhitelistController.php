28a2640f808ea9f662932bcc0b11cf0b97fbcdb3
Joas Schilling <coding@schilljs.com>
2025-10-23 09:33:40 +0200

    feat(add): Allow overwriting an entry
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Controller/IPWhitelistController.php
+++ lib/Controller/IPWhitelistController.php
@@ -68,22 +68,28 @@ class IPWhitelistController extends Controller {
 			return new JSONResponse([], Http::STATUS_BAD_REQUEST);
 		}
 
-		$keys = $this->appConfig->getKeys(Config::APPID);
-		$keys = array_filter($keys, static function (string $key) {
-			return str_starts_with($key, Config::ALLOWLIST_PREFIX) && !str_ends_with($key, Config::COMMENT_SUFFIX);
-		});
+		$newValue = $ip . '/' . $mask;
+		$configMap = $this->appConfig->getAllValues(Config::APPID, Config::ALLOWLIST_PREFIX);
 
 		$id = 0;
-		foreach ($keys as $key) {
-			$tmp = (int)substr($key, strlen(Config::ALLOWLIST_PREFIX));
-			if ($tmp > $id) {
-				$id = $tmp;
+		$maxId = 0;
+		foreach ($configMap as $key => $value) {
+			$tmpId = (int)substr($key, strlen(Config::ALLOWLIST_PREFIX));
+			if ($value === $newValue && !str_ends_with($key, Config::COMMENT_SUFFIX)) {
+				$id = $tmpId;
+				break;
+			}
+
+			if ($tmpId > $maxId) {
+				$maxId = $tmpId;
 			}
 		}
-		$id++;
 
-		$value = $ip . '/' . $mask;
-		$this->appConfig->setValueString(Config::APPID, Config::ALLOWLIST_PREFIX . $id, $value);
+		if ($id === 0) {
+			$id = $maxId + 1;
+		}
+
+		$this->appConfig->setValueString(Config::APPID, Config::ALLOWLIST_PREFIX . $id, $newValue);
 		$comment = trim($comment);
 		if ($comment !== '') {
 			$this->appConfig->setValueString(Config::APPID, Config::ALLOWLIST_PREFIX . $id . Config::COMMENT_SUFFIX, $comment);
